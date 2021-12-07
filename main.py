6ff20db85f688836cb2c61eb827145d7bd1f1fb7
Chandra Prakash <95275695+DPrakashM@users.noreply.github.com>
2021-12-07 09:27:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-import flask
 from flask import Flask,render_template,request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-app = Flask(__name__)
 
-chatbot = ChatBot('Zoro',storage_adapter="chatterbot.storage.SQLStorageAdapter")
+app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+chatbot = ChatBot('Ron Obvious',storage_adapter="chatterbot.storage.SQLStorageAdapter")
 # Create a new trainer for the chatbot
 trainer = ChatterBotCorpusTrainer(chatbot)
 # Train the chatbot based on the english corpus
 trainer.train("chatterbot.corpus.english")
 
+
+# Creating model table for our CRUD database
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 @app.route("/get")
 def chatbotcall():
     msg = request.args.get('msg')
     response = chatbot.get_response(msg)
-    return render_template('index.html',ourresponse=str(msg),botresponse=response)
+    return render_template('index1.html',ourresponse=str(msg),botresponse=response)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
